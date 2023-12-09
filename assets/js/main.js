fadf39179d368052e4893e25b144207b83203912
Kerensmjtk <kerensimanjuntak123@gmail.com>
2023-12-09 09:27:07 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  
+    $("#slider-hero").owlCarousel({
+        loop: true,
+        nav:true,
+        items: 1,
+        navText: [
+            "<i class='fa fa-angle-left'><i>",
+            "<i class='fa fa-angle-right'><i>"
+        ],
+        navContainer: "#slider-hero-nav",
+    });
+
+
+
+
+
+
+
+
+
+
+})
